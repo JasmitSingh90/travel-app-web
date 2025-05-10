c95dad1c3a8808cb6b2f7536a20abdd4d95a097f
JasmitSingh90 <jasmitsingh90@gmail.com>
2025-05-11 04:49:26 +0530

    Updated the site
    Updated the site

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "418644252"
+    serviceWorkerVersion: "3720121508"
   }
 });
