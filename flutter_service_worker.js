f9b9c13cd0fb4212517e9c8a98ab07b7ef2444d5
JasmitSingh90 <jasmitsingh90@gmail.com>
2025-07-09 16:52:56 +0530

    Fixed issues
    Fixed issues

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9162a0ec2bd42c09cc8e015c4cdd6d14",
+const RESOURCES = {"flutter_bootstrap.js": "2ad12cb87f44d4a732d84f2552793ddf",
 "version.json": "b2c5b71b0ac8933fd453ef5a8cd7891d",
 "index.html": "07c3af783ea5818ccb1c4fc7e7b859e4",
 "/": "07c3af783ea5818ccb1c4fc7e7b859e4",
-"main.dart.js": "4a1e318d5ad1db171f134363c6404a79",
+"main.dart.js": "0175f75b58b5b254bf6b1b2e088b945a",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "9ec3cdc0277dd02c19db0fcc625a2e6a",
 "icons/Icon-192.png": "f227577269e6a65b6b0f2c30f07f2f48",
