467f9abf7b9380f5d978611053cf9e25e795323d
ravi-gajjar-dev <ravigajjar932@gmail.com>
2025-07-15 20:59:42 +0530

    Deploy version 1.0.0+69

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0b2400353ad36dc2c32c24e38cb3e990",
-"version.json": "eebf8651f86d7e382abf5adfaf63c415",
+const RESOURCES = {"flutter_bootstrap.js": "22f24c6b155b7c8d7eb4284cda12c3be",
+"version.json": "37d700c982223808bd8ede4f638eee4a",
 "index.html": "e7b8123612040ff1a65fd41941b0336c",
 "/": "e7b8123612040ff1a65fd41941b0336c",
 "js/web_audio_recorder.js": "d16b4dad32b8aa2f42df8d34729a17f3",
-"main.dart.js": "52cd11c4631c0023bc43fffc29778f33",
+"main.dart.js": "0d70dc9cbcd019ea6f455637d9cac8e6",
 "404.html": "131d1ac2eb79a07ab55360f186bea26b",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "9ec3cdc0277dd02c19db0fcc625a2e6a",
